9a84c0fced0232688fd4e2b193df4dac86a78c0e
hisdream86 <hisdream86@gmail.com>
2022-05-01 12:03:16 +0900

    feat: register `Product` model to django admin

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from product import models
 
-# Register your models here.
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "description", "price")
+    list_display_links = ("id",)
